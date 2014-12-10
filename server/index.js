f98b2ed84ed3139012314e6c933e9f6892f37b10
Chyld Medford <chyld.medford@gmail.com>
2014-12-09 18:02:51 -0600

    cleaned up index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 'use strict';
 
 var Hapi         = require('hapi'),
-  server         = new Hapi.Server('0.0.0.0', process.env.PORT, {cors:{
-                                                                        origin: ['http://localhost:8100'],
-                                                                        credentials: true
-                                                                      }}),
+  server         = new Hapi.Server('0.0.0.0', process.env.PORT, {cors:{origin: ['http://localhost:8100'],credentials: true}}),
   routes         = require('./routes/config/routes'),
   plugins        = require('./routes/config/plugins'),
   authentication = require('./routes/config/authentication');
